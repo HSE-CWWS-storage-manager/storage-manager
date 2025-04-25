b3cd6e47e75db9f6ffa4bc5976857c753a8f325b
Константин <dropinclass1@gmail.com>
2025-04-25 17:40:26 +0500

    test: basic register unit tests

--- backend.Tests/AccountServiceTest.cs
+++ backend.Tests/AccountServiceTest.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using AutoMapper;
+using backend.Dtos.Request;
+using backend.Mapping;
+using backend.Mapping.Impl;
+using backend.Services.Impl;
+using Microsoft.AspNetCore.Identity;
 using Moq;
 
 namespace backend.Tests;
@@ -15,9 +20,13 @@ public class AccountServiceTest
         userManagerMock.Object.UserValidators.Add(new UserValidator<IdentityUser>());
         userManagerMock.Object.PasswordValidators.Add(new PasswordValidator<IdentityUser>());
         
-        userManagerMock.Setup(userManager => userManager.CreateAsync(It.IsAny<IdentityUser>()))
-            .ReturnsAsync(IdentityResult.Success)
-            .Callback<IdentityUser, string>((x, y) => dict.Add(x.UserName!, new(x, y)));
+        userManagerMock.Setup(userManager => userManager.CreateAsync(It.IsAny<IdentityUser>(), It.IsAny<string>()))
+            .ReturnsAsync((IdentityUser user, string _) => dict.ContainsKey(user.Email!) ? IdentityResult.Failed() : IdentityResult.Success)
+            .Callback<IdentityUser, string>((x, y) =>
+            {
+                if (!dict.ContainsKey(x.Email!))
+                    dict.Add(x.UserName!, new(x, y));
+            });
 
         userManagerMock.Setup(userManager => userManager.FindByNameAsync(It.IsAny<string>()))
             .ReturnsAsync((string name) => dict[name].Item1);
@@ -25,8 +34,54 @@ public class AccountServiceTest
         return userManagerMock;
     }
 
+    public static IMapper CreateAutoMapper()
+    {
+        var configuration = new MapperConfiguration(configuration => configuration.AddProfile<MappingProfile>());
+
+        return configuration.CreateMapper();
+    }
+
     [Fact]
     public async Task Register_ValidUserData_ReturnsValidUser()
     {
+        var accountService = new AccountService(CreateAutoMapper(), new UserMapper(), MockUserManager().Object);
+
+        var request = new UserRegistrationRequest
+        {
+            Email = "test@example.com",
+            Password = "12345678Aa$",
+            ConfirmPassword = "12345678Aa$"
+        };
+        
+        var actual = await accountService.Create(request);
+        
+        Assert.True(actual.Item2.Succeeded);
+        Assert.NotNull(actual.Item1);
+        Assert.Equal("test@example.com", actual.Item1.Email);
+    }
+    
+    [Fact]
+    public async Task Register_Conflict_ReturnsUnsuccessfulResult()
+    {
+        var accountService = new AccountService(CreateAutoMapper(), new UserMapper(), MockUserManager().Object);
+
+        var request = new UserRegistrationRequest
+        {
+            Email = "test@example.com",
+            Password = "12345678Aa$",
+            ConfirmPassword = "12345678Aa$"
+        };
+        
+        var request2 = new UserRegistrationRequest
+        {
+            Email = "test@example.com",
+            Password = "12345678Aa$",
+            ConfirmPassword = "12345678Aa$"
+        };
+
+        await accountService.Create(request);
+        var actual = await accountService.Create(request2);
+        
+        Assert.False(actual.Item2.Succeeded);
     }
 }
