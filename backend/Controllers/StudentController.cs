eac9d6f360cb5cb79abfdae4dfd5bdfbd657647f
Константин <dropinclass1@gmail.com>
2025-05-21 23:28:54 +0500

    feat(student): add student controller

---
+++ backend/Controllers/StudentController.cs
@@ -0,0 +1,38 @@
+﻿using backend.Services;
+using backend.Utils;
+using common.Dtos.Request;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+[Authorize(Policy = StringConstants.EditorPolicy)]
+public class StudentController(IStudentService studentService) : ControllerBase
+{
+
+    [HttpPost]
+    public async Task<IActionResult> Create(StudentCreateRequest request)
+    {
+        return !ModelState.IsValid ? Problem() : Ok(await studentService.Create(request));
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> Find([FromQuery] StudentFindRequest request)
+    {
+        return !ModelState.IsValid ? Problem() : Ok(await studentService.Find(request));
+    }
+    
+    [HttpPatch]
+    public async Task<IActionResult> Update(StudentUpdateRequest request)
+    {
+        return !ModelState.IsValid ? Problem() : Ok(await studentService.Update(request));
+    }
+    
+    [HttpPost("Delete")]
+    public async Task<IActionResult> Delete(StudentDeleteRequest request)
+    {
+        return !ModelState.IsValid ? Problem() : Ok(await studentService.Delete(request));
+    }
+}
