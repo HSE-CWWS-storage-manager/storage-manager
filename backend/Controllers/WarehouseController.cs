ed4c9c06e8a5b7da6cd0d8506bc3e921cfeb4ff6
Константин <dropinclass1@gmail.com>
2025-06-23 23:27:20 +0500

    chore: return raw warehouse methods

--- backend/Controllers/WarehouseController.cs
+++ backend/Controllers/WarehouseController.cs
@@ -71,4 +71,28 @@ public class WarehouseController(IWarehouseService service) : ControllerBase
     {
         return !ModelState.IsValid ? Problem() : Ok(service.ListWarehouses(request));
     }
+    
+    [HttpPost("AddOnStock")]
+    public async Task<IActionResult> AddOnStock(ChangeEquipmentUnitCountRequest request)
+    {
+        return !ModelState.IsValid ? Problem() : Ok(await service.AddEquipmentOnStock(request));
+    }
+    
+    [HttpPost("ExtractFromStock")]
+    public async Task<IActionResult> ExtractFromStock(ChangeEquipmentUnitCountRequest request)
+    {
+        return !ModelState.IsValid ? Problem() : Ok(await service.ExtractEquipmentFromStock(request));
+    }
+    
+    [HttpPost("AddOnLoan")]
+    public async Task<IActionResult> AddOnLoan(ChangeEquipmentUnitCountRequest request)
+    {
+        return !ModelState.IsValid ? Problem() : Ok(await service.AddEquipmentOnLoan(request));
+    }
+    
+    [HttpPost("ExtractFromLoan")]
+    public async Task<IActionResult> ExtractFromLoan(ChangeEquipmentUnitCountRequest request)
+    {
+        return !ModelState.IsValid ? Problem() : Ok(await service.ExtractEquipmentFromLoan(request));
+    }
 }
